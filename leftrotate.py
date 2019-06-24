8be41b23aa7c60cd5953bc01d69532fe5fb18fb8
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2019-06-24 18:06:49 +0530

    Update leftrotate.py

--- leftrotate.py
+++ leftrotate.py
@@ -1,29 +1,10 @@
-def leftRotate(arr, d, n): 
-    g_c_d = gcd(d, n) 
-    for i in range(g_c_d):   
-        temp = arr[i] 
-        j = i 
-        while 1: 
-            k = j + d 
-            if k >= n: 
-                k = k - n 
-            if k == i: 
-                break
-            arr[j] = arr[k] 
-            j = k 
-        arr[j] = temp 
-  
- 
-def printArray(arr, size): 
-    for i in range(size): 
-        print (arr[i],end =" ") 
+n,d=map(int,input().split())
+arr=list(map(int,input().split()))[:n]
+for i in range(0,d):
+    temp=arr[0]
+    for j in range(0,n-1):
+        arr[j]=arr[j+1]
+    arr[n-1]=temp
+for i in range(0,n):
+    print(arr[i],end=" ") 
 
-def gcd(a, b): 
-    if b == 0: 
-        return a; 
-    else: 
-        return gcd(b, a % b) 
-n,d=map(int,input().split()) 
-arr =list(map(int,input().split()))[:n]
-leftRotate(arr, d, n) 
-printArray(arr, n) 
